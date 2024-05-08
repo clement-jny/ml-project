5cd25ac0f6105ba8f7a68c9e716261f791ec9720
clement-jny <82049263+clement-jny@users.noreply.github.com>
2024-05-09 01:43:15 +0200

    update

--- scripts/add.py
+++
@@ -1,1 +0,0 @@
-# # Add new data here JSON format (title, body), train model, evaluate model, save model, populate database
--- scripts/main.py
+++ scripts/main.py
@@ -21,6 +21,9 @@ from sentence_transformers import SentenceTransformer, util
 from datasets import load_dataset
 from transformers import BertTokenizer, BertModel
 from indexMapping import indexMapping
+from fastapi import FastAPI, Body
+# from typing import Union
+from pydantic import BaseModel
 
 
 # # Load env var & connect to Elasticsearch # #
@@ -53,7 +56,7 @@ print("-------------------")
 
 
 wikipedia_dataset = load_dataset('wikipedia', '20220301.fr', split='train', trust_remote_code=True)
-small_train_dataset = wikipedia_dataset.select(range(50))
+small_train_dataset = wikipedia_dataset.select(range(15))
 # print(small_train_dataset[0])
 
 # print(small_train_dataset)
@@ -94,48 +97,52 @@ for article in small_train_dataset:
 		'text_embeddings': article['text_embeddings']
 	})
 
-
-# try:
-# 	esClient.indices.create(index='wiki_article', mappings=indexMapping)
-# except Elasticsearch.exceptions.RequestError as ex:
-# 	if ex.error == 'resource_already_exists_exception':
-# 		pass # Index already exists. Ignore.
-# 	else: # Other exception - raise it
-# 		raise ex
+es_index = 'wiki_article'
+try:
+	esClient.indices.create(index=es_index, mappings=indexMapping)
+except RequestError as ex:
+	if ex.error == 'resource_already_exists_exception':
+		pass # Index already exists. Ignore.
+	else: # Other exception - raise it
+		raise ex
 
 for article in article_json:
 	try:
-		esClient.index(index='wiki_article', document=article, id=article['id'])
+		esClient.index(index=es_index, document=article, id=article['id'])
 	except Exception as e:
 		print(e)
 
-print(esClient.count(index='wiki_article'))
+
+print('Count article -', esClient.count(index=es_index))
 
 # TODO
 # Create util class to convert Dataset <-> json
 # Create esClient class w/ connection, index creation & CRUD
 
+
+
+
 # # Try with a dump query -> Get 5/10 sim (knn) article # #
-input_keywords = "Algorithmes"
-encoded_query = model.encode(input_keywords)
+# input_keywords = "Algorithmes"
+# encoded_query = model.encode(input_keywords)
 
-query = {
-	'field': 'text_embeddings',
-	'query_vector': encoded_query,
-	'k': 5,
-	'num_candidates': len(small_train_dataset)
-}
+# query = {
+# 	'field': 'text_embeddings',
+# 	'query_vector': encoded_query,
+# 	'k': 5,
+# 	'num_candidates': len(small_train_dataset)
+# }
 
 
-res = esClient.knn_search(index='wiki_article', knn=query, source=['title', 'text'])
+# res = esClient.knn_search(index=es_index, knn=query, source=['title', 'text'])
 # print(res['hits']['hits'])
 
-print("-------------------")
-print('Dataset sie:', len(small_train_dataset))
-print("Query:", input_keywords)
-print("Results:")
-for hit in res['hits']['hits']:
-	print(hit['_source']['title'], '-', hit['_score'])
+# print("-------------------")
+# print('Dataset size:', len(small_train_dataset))
+# print("Query:", input_keywords)
+# print("Results:")
+# for hit in res['hits']['hits']:
+# 	print(hit['_source']['title'], '-', hit['_score'])
 
 
 
@@ -144,4 +151,66 @@ for hit in res['hits']['hits']:
 
 # # Get real query from web interface / console interface # #
 
+# # Define endpoint to get query from frontend # #
+app = FastAPI()
 
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World!"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+
+# # /search endpoint | POST - body: {query: str} # #
+@app.post("/search")
+def search(query: str = Body(..., embed=True)):
+	# Encode the query
+	encoded_query = model.encode(query)
+
+	# Perform the search
+	query = {
+		'field': 'text_embeddings',
+		'query_vector': encoded_query,
+		'k': 5,
+		'num_candidates': len(small_train_dataset)
+	}
+
+	# Get the results
+	res = esClient.knn_search(index=es_index, knn=query, source=['title', 'text'])
+
+	print(res['hits']['hits'])
+
+	# Return the results
+	proba = hit['_source']['title'] + ' - ' + hit['_score']
+	return { "query": query, "results": res['hits']['hits'], "proba": proba }
+
+# # /add endpoint | POST - body: {data: {title: str, text: str}} # #
+# # Add new data here JSON format (title, body), encode title & body, populate database
+class NewData(BaseModel):
+	title: str
+	text: str
+
+@app.post("/add")
+def add(data: NewData):
+	# Encode the data
+	encoded_title = model.encode(data.title)
+	encoded_text = model.encode(data.text)
+
+	# Insert into ES
+	article = {
+		'title': data.title,
+		'text': data.text,
+		'title_embeddings': encoded_title,
+		'text_embeddings': encoded_text
+	}
+
+	try:
+		esClient.index(index=es_index, document=article)
+	except Exception as e:
+		print(e)
+
+	return { "status": "success" }
